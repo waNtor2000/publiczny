82e03040bc60545c880ab5322496f85bfbcee4e0
waNtor2000 <mati.chudes@gmail.com>
2023-02-01 08:04:44 +0100

    Update check_acces_list.js

--- htacces/check_acces_list.js
+++ htacces/check_acces_list.js
@@ -1,5 +1,13 @@
+/* 
+  Author : CdChudes
+  Desc : checking JSON list for acces player od ID
+  Version: 0.1
+  Date : 1.02.2023
+*/
+
+
 $.ajax({
-    url: "test.json",
+    url: "list.json",
     dataType: "json",
     success: function(data) {
       console.log(data);
