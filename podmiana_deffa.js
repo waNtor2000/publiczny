b923b0f30c6801a10bffff5397173acdec358c97
waNtor2000 <mati.chudes@gmail.com>
2023-03-16 13:04:49 +0100

    Dodaj jeszcze wyswietlanie

--- podmiana_deffa.js
+++ podmiana_deffa.js
@@ -13,38 +13,39 @@
 (function() {
     'use strict';
     console.log('Start');
-    var info= [];
+    var info = [];
 
-    function add_troops_info(){
-        var data = $('#units_table tBody tr.row_a, tr.row_b');
-        for(var i=0; i< data.length; i++){
-            var village = {};
-            console.log(i);
-            console.log(data[i].getElementsByClassName('village_anchor contexted')[0].outerText.trim().slice(-12,-5));   
-            console.log(data[i].className);
-            village.coord = data[i].getElementsByClassName('village_anchor contexted')[0].outerText.trim().slice(-12,-5); 
-            village.spear = parseInt(data[i].children[2].outerText)
-            village.sword = parseInt(data[i].children[3].outerText)
-            village.archer = parseInt(data[i].children[5].outerText)
-            village.spy = parseInt(data[i].children[6].outerText)
-            village.heavy = parseInt(data[i].children[9].outerText)
-
-            info.push(village);
+    function add_troops_info() {
+    var data = $('#units_table tBody tr.row_a, tr.row_b');
+    for (var i = 0; i < data.length; i++) {
+        var village = {};
+        village.coord = data[i].getElementsByClassName('village_anchor contexted')[0].outerText.trim().slice(-12, -5);
+        village.spear = parseInt(data[i].children[2].outerText);
+        village.sword = parseInt(data[i].children[3].outerText);
+        village.archer = parseInt(data[i].children[5].outerText);
+        village.spy = parseInt(data[i].children[6].outerText);
+        village.heavy = parseInt(data[i].children[9].outerText);
+        // check if village already exists in info array
+        var index = info.findIndex(v => v.coord === village.coord);
+        if (index !== -1) {
+        // if village exists, add troops to existing object
+        info[index].spear += village.spear;
+        info[index].sword += village.sword;
+        info[index].archer += village.archer;
+        info[index].spy += village.spy;
+        info[index].heavy += village.heavy;
+        } else {
+        // otherwise, add new village object to info array
+        info.push(village);
         }
     }
-// dokończyć tutaj zeby dodawało
-    function check_village_on_list(kordy, kordyListy){
-        for(i = 0; i<info.length;i++){
-            if(kordy[i]==kordyListy){
-                console.log(i);
-            }
-        }
     }
+// dokończyć tutaj zeby dodawało i jeśli już występuje to nie tworzy nowego obiektu a dodaje wartości do obiektów
+
 
 
 
    add_troops_info();
-   check_village_on_list(info.coord)
    console.log(info);
 
 })();
